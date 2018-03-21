ee50124f187fd0ca697dfbcfcca17b100ec15f9a
aassdd113355 <aubowenli@mail.scut.edu.cn>
2018-03-21 15:53:18 +0800

    增加8s内屏蔽关水功能并完成测试

---
+++ SCUT_Camera0321/HARDWARE/EXTI/exti.c
@@ -0,0 +1,96 @@
+#include "exti.h"
+#include "delay.h" 
+#include "ov7670.h"
+#include "led.h"
+#include "Pic_Process.h"
+#include "usart.h"	
+u8 ov_sta;
+extern volatile u8 ThereIsACircle; //Pic_Process.c中定义
+extern volatile u8 timeIsUp;
+extern u16 AddWaterTime;
+extern u8 AddWaterNum;
+u8 Processing = 0;
+ //外部中断5~9服务程序，读取最新一帧的图像
+void EXTI9_5_IRQHandler(void)
+{		 		
+	if(EXTI->PR&(1<<9))			//是9线的中断
+	{     
+		EXTI->PR=1<<9;     //清除LINE9上的中断标志位	
+
+				OV7670_WRST=0;	 	//复位写指针	
+				delay_us(1);
+				OV7670_WRST=1;	
+				OV7670_WREN=1;		//允许写入FIFO
+				ov_sta++;
+				delay_us(200);
+	}
+						  
+} 
+ //3线中断服务函数
+void EXTI3_IRQHandler(void)
+{		
+	if(EXTI->PR&(1<<3))			//是3线的中断
+	{ 
+		EXTI->PR=1<<3;     //清除LINE3上的中断标志位	
+		delay_ms(500);
+		if(GlassArea == 1)
+		{
+			Glass_Light_On = 0;
+			Light_On=0; //灯光关闭
+			On_Off = 0; 	//关
+			ThereIsACircle = 0;
+//			AddWaterNum = 0;
+			Processing = 0;
+			Light_On_Glass_Water = 0; //水灯关闭
+			timeIsUp = 0; 						//定时器4清零
+		}
+		else if(Processing == 0 && GlassArea == 0)
+		{		
+				
+//				TIM4->CR1&=0x00;							//关闭定时器
+//				TIM4->CNT = 0;               //计数器清零			
+				TIM2->CNT = 0;
+			  timeIsUp = 0;
+			  AddWaterTime = 0;
+			if(GlassAreaAssitSignal == 0) //非透明杯
+			{					
+								Light_On=1; 			//灯光打开
+							  printf("0"); 
+			}else if(GlassAreaAssitSignal == 1) //透明杯
+			{
+								Glass_Light_On = 1;
+								printf("1"); 
+			}
+
+				TIM3->CR1|=0x01;	//使能定时器
+
+				//delay_ms(100);		//清除第一次中断
+				//TIM3->SR&=~(1<<0);	//清除中断标志位	
+
+			
+//				TIM3->CNT = 0;
+//				TIM3->EGR = 1;
+//				while((TIM3->SR&0x01) == 0)
+//				;
+//		  	TIM3->SR&=~(1<<0);	//清除中断标志位	
+//				TIM3->DIER|=1<<0;   //允许更新中断
+		}
+	} 
+}
+
+
+
+
+//外部中断9初始化
+void EXTI9_Init(void)
+{												  
+	Ex_NVIC_Config(GPIO_F,9,RTIR); 			//上升沿触发			  
+	MY_NVIC_Init(0,0,EXTI9_5_IRQn,2);		//抢占0,子优先级0，组2	   
+}
+
+void EXTIX_Init(void)	//外部中断初始化	
+{	
+	Ex_NVIC_Config(GPIO_D,3,3); 			//2018.1.17改
+	MY_NVIC_Init(1,0,EXTI3_IRQn,2);		//抢占1,子优先级0，组2	
+}
+
---
+++ SCUT_Camera0321/USER/main.c
@@ -0,0 +1,118 @@
+#include "sys.h"
+#include "delay.h"  
+#include "usart.h"  
+#include "ov7670.h"
+#include "timer.h"	  		 
+#include "sccb.h"	
+#include "exti.h"
+#include "Pic_Process.h"
+#include "dma.h"
+#include "led.h"
+void dynamic_check_Glass(void);
+void dynamic_check(void);
+u8 timeIsUp; //¶¨Ê±Æ÷Ê±¼äÓÃ¾¡±êÖ¾£¨µ¥´Î¼ÓË®³¬Ê±±êÖ¾£©
+u16 AddWaterTime; //×Ü¼ÓË®Ê±¼äÊý
+extern u8 ov_sta;	//ÔÚexit.cÀïÃæ¶¨Òå
+extern int flag;	//ÔÚexit.cÀïÃæ¶¨Òå
+extern volatile u8 ThereIsACircle;
+extern int static_flag1, static_flag2; //ÔÚpro.cÖÐ¶¨Òå
+
+//³õÊ¼µÚÒ»´Î½øÐÐ¾²Ì¬¼ì²â£»µ±¾²Ì¬¼ì²â³öË®Î»Îª-1Ê±¼ÌÐø¼ì²â£»¼ì²â³öË®Î»>6Ê±¼ÌÐø¼ì²â£»
+//µ±¾²Ì¬¼ì²â³öË®Î»ÔÚ0-6µÄ¿É¼ÓË®µÄ·¶Î§ÄÚÊ±£¬¿ªÆô¶¯Ì¬¼ì²âºÍ¿ªË®ÐÅºÅ£»
+//µ±¶¯Ì¬¼ì²â³öË®Î»Îª0-6Ê±¼ÌÐø¼ì²â£»¼ì²â³öË®Î»>6Ê±·µ»Ø¾²Ì¬¼ì²â£¬²¢¹Ø¶Ï¼ÓË®ÐÅºÅ¡£
+int main(void)
+{   
+	cameraSysInit();	//ÏµÍ³³õÊ¼»¯
+	 
+
+ 	while(1) 
+	{		
+		
+			
+		while(!ThereIsACircle);    			//Ã»¼ì²â±­¿ÚµÄÊ±ºò×èÈûÔÚÕâÀï	
+		
+		
+
+			//·Ç²£Á§±­
+		if(GlassArea == 0 && GlassAreaAssitSignal == 0)
+		{
+				CAMERA_Image_Cut_Compress_6080(0,0);//	»ñÈ¡Ò»Ö¡Í¼Æ¬£¬²¢ÇÒ´æÔÚÊý×é       2017-07-13 By BachMan			
+				Image_Histeq();
+				Sobel_After();
+				eraseHorizon();
+//			OPTA();
+				Water_Level_Static();
+				Image_Send_After_Static();			
+						
+//				while(max>=0 && max< 6 && GlassArea == 0 && timeIsUp == 0 && AddWaterTime <= 50000 && AddWaterNum < 10)
+//				while(max>=0 && max< 6 && GlassArea == 0 && timeIsUp == 0 && AddWaterTime <= 50000)
+
+				while(max>=0 && max< 5 && GlassArea == 0)
+				{
+					On_Off = 1;//¼ÓË®ÐÅºÅ¿ª
+					
+					TIM2->CR1|=0x01; //¿ªÆô¶¨Ê±Æ÷
+					dynamic_check();
+				}		
+
+					
+		}else if(GlassArea == 0 && GlassAreaAssitSignal == 1){
+							CAMERA_Image_Cut_Compress_6080(0,0);//	»ñÈ¡Ò»Ö¡Í¼Æ¬£¬²¢ÇÒ´æÔÚÊý×é       2017-07-13 By BachMan			
+							Image_Histeq();
+							Sobel_After_Glass();
+							eraseHorizon();
+//						  OPTA();
+							Glass_Water_Level_Static();
+							Image_Send_After_Static();	
+							
+				while(max>=0 && max< 6 && GlassArea == 0)
+				{
+					On_Off = 1;//¼ÓË®ÐÅºÅ¿ª
+					TIM2->CR1|=0x01; //¿ªÆô¶¨Ê±Æ÷
+					
+					dynamic_check_Glass();
+				}					
+		}
+	}	
+}
+//	//¶¯Ì¬¼ì²â ¾²Ì¬½áÊøºó
+void dynamic_check()
+{
+				CAMERA_Image_Cut_Compress_6080(0,0);
+				Image_Histeq();
+				Sobel_After();
+				Water_Level_Dynamic();	
+				Image_Send_Dynamic();
+//				AddWaterTime += TIM2->CNT;	 //ÀÛ¼Æ×Ü¼ÓË®Ê±¼ä
+//				TIM2->CNT = 0;               //¼ÆÊýÆ÷ÇåÁã
+				if(max_dynamic > 5 && timeIsUp == 1) //|| AddWaterTime > 50000 3.20¸Ä
+				{
+					static_flag2 = -1;			//Çå³ý¾²Ì¬¼ÓË®ÖÐµÄ»º´æ
+					static_flag1 = -1;
+					//AddWaterNum = AddWaterNum + 1;
+					On_Off = 0;//¹Ø¶Ï¼ÓË®ÐÅºÅ
+					max = 6;	//Á¬½Ó¶¯Ì¬¾²Ì¬¼ì²
+					static_flag1 = 6;		
+//					TIM2->CR1&=0x00;
+				}				
+}
+
+void dynamic_check_Glass(){
+				CAMERA_Image_Cut_Compress_6080(0,0);
+				Image_Histeq();
+				Sobel_After_Glass();
+				Glass_Water_Level_Dynamic();	
+				Image_Send_Dynamic();
+//				AddWaterTime += TIM2->CNT;	 //ÀÛ¼Æ×Ü¼ÓË®Ê±¼ä
+//				TIM2->CNT = 0;               //¼ÆÊýÆ÷ÇåÁã
+				if(max_dynamic >= 5 && timeIsUp == 1) //|| AddWaterTime > 50000 3.20¸Ä
+				{
+					static_flag2 = -1;				//Çå³ý¾²Ì¬¼ÓË®ÖÐµÄ»º´æ
+					static_flag1 = -1;
+					//AddWaterNum = AddWaterNum + 1;
+					On_Off = 0;//¹Ø¶Ï¼ÓË®ÐÅºÅ
+					max = 6;	//Á¬½Ó¶¯Ì¬¾²Ì¬¼ì²
+					static_flag1 = 6;		
+//					TIM2->CR1&=0x00;
+				}				
+}
